02e26f17ebe0d1c89352df1a9c64a2d8f69cd9f5
owieth <olivier.winkler@icloud.com>
2022-10-29 13:55:01 +0100

    feat(membercard): initial membercard setup

--- contracts/MemberCard.sol
+++ contracts/MemberCard.sol
@@ -1,12 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.16;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+// Interfaces
 import "./interfaces/IMemberCard.sol";
 
-// Import this file to use console.log
-import "hardhat/console.sol";
+contract MemberCard is
+    IMemberCard,
+    ERC721URIStorage,
+    ERC721Enumerable,
+    ERC721Burnable
+{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-
-contract MemberCard is IMemberCard {
-
+    constructor() ERC721("MemberCard", "SWSS") {}
 }
