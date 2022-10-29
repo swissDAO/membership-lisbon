e9ce17a549c87c27fa28c60b1b579275edf1f2e0
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-10-29 17:27:15 +0200

    created createEvent, updateEvent

--- contracts/Dispatcher.sol
+++ contracts/Dispatcher.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.16;
 
 import "./interfaces/IDispatcher.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 
 // Import this file to use console.log
 import "hardhat/console.sol";
 
 
-contract Dispatcher is IDispatcher, isOwnable {
+contract Dispatcher is IDispatcher, Ownable {
 
     struct Member {
         address member;
@@ -17,10 +19,12 @@ contract Dispatcher is IDispatcher, isOwnable {
     struct Event {
         string title;
         uint256 id;
-        uint256 timestamp;
-        address[] owners;
+        uint256 timestampOfEvent;
+        address owner;
         Member[] attendees;
     }
+
+    uint256 eventCount;
 
     mapping(uint256 => Event) public idToEvents;
     mapping(uint256 => Member) public idToMember;
@@ -29,22 +33,42 @@ contract Dispatcher is IDispatcher, isOwnable {
 
     }
 
-    function createEvent() public {
-        owners.push(msg.sender);
-        owners.push(owner());
+    function createEvent(string memory _title, uint256 _timestampOfEvent) public {
+        eventCount++;
+
+        Member[] memory emptyAttendees;
+
+        Event memory swissDAOEvent = Event({
+                                        title: _title,
+                                        id: eventCount,
+                                        timestampOfEvent: _timestampOfEvent,
+                                        owner: msg.sender,
+                                        attendees: emptyAttendees
+        });
+
+        idToEvents[eventCount] = swissDAOEvent;
     }
 
-    function updateEventTitle(uint256 _EventId, string memory _newTitle) public {
+    function updateEvent(
+        uint256 _EventId,
+        string memory _newTitle,
+        uint256 _timestampOFEvent
+        ) public {
         // require owner calls this function
-        // what should be the purpose of this function? update owner?
-        // Event storage swissDAOEvent = idToEvents[_EventId];
+        
         Event memory swissDAOEventCopy = idToEvents[_EventId];
-        Event swissDAOEvent = Event(
-                                        swissDAOEventCopy.attendees,
-                                        swissDAOEventCopy.id,
-                                        swissDAOEventCopy.owners,
-                                        _newTitle
-                                    );
+
+        require(msg.sender == swissDAOEventCopy.owner, "You are not the owner of this event");
+        
+
+        Event memory swissDAOEvent = Event({
+                                        title: _newTitle,
+                                        id: _EventId,
+                                        timestampOfEvent: _timestampOFEvent,
+                                        owner: swissDAOEventCopy.owner,
+                                        attendees: swissDAOEventCopy.attendees
+                                    });
+
         idToEvents[_EventId] = swissDAOEvent;
     }
 
--- contracts/MemberCard.sol
+++ contracts/MemberCard.sol
@@ -20,7 +20,7 @@ contract MemberCard is
 
     constructor() ERC721("MemberCard", "SWSS") {}
 
-    function mintMemberCard() override {
+    function mintMemberCard() public {
         uint256 newItemId = _tokenIds.current();
 
         _safeMint(msg.sender, newItemId);
@@ -30,15 +30,15 @@ contract MemberCard is
         _tokenIds.increment();
     }
 
-    function updateSkills() {
-      ERC721 _memberCard = ERC721()
+    function updateSkills() public {
+      ERC721 _memberCard = ERC721();
     }
 
-    function earnExperience() {
-      ERC721 _memberCard = ERC721()
+    function earnExperience() public {
+      ERC721 _memberCard = ERC721();
     }
 
     function getTokenURI() private view override returns (string memory) {
-      return "https://gateway.pinata.cloud/ipfs/QmRj91zjBaMjUHtxJnGh32UvM6Wtd4fXNXt8czABLuuCsE"
+      return "https://gateway.pinata.cloud/ipfs/QmRj91zjBaMjUHtxJnGh32UvM6Wtd4fXNXt8czABLuuCsE";
     }
 }
